5ded85f4ce12b112a8998a20f30335bce7a59e28
Hiali <hialidias@gmail.com>
2023-09-06 02:18:31 -0300

    first commit

---
+++ src/components/decorators/SmartDeviceDecorators.ts
@@ -0,0 +1,58 @@
+import { QueryRowFormat } from "@itwin/core-common";
+import { Decorator, IModelConnection, Marker, ScreenViewport, DecorateContext } from "@itwin/core-frontend";
+import { SmartDeviceMarker } from "../markers/SmartDeviceMarker";
+import { SmartDeviceApi } from "../../SmartDeviceApi";
+
+
+export class SmartDeviceDecorator implements Decorator {
+
+   private _iModel: IModelConnection;
+   private _markerSet: Marker[];
+
+
+   constructor(vp: ScreenViewport) {
+      this._iModel = vp.iModel;
+      this._markerSet = [];
+
+      this.addMarkers();
+   }
+   private async getSmartDeviceData() {
+      const query = `SELECT SmartDeviceId, 
+      SmartDeviceType,
+      ECInstanceId,
+      Origin
+      FROM DgnCustomItemTypes_HouseSchema.SmartDevice
+      WHERE Origin IS NOT NULL`
+      const results = this._iModel.query(query, undefined, { rowFormat: QueryRowFormat.UseJsPropertyNames });
+      const values = [];
+
+      for await (const row of results) values.push(row);
+       return values;
+   }
+
+
+   private async addMarkers() {
+      const values = await this.getSmartDeviceData();
+      const cloudData = await SmartDeviceApi.getData();
+
+      values.forEach(value => {
+         const SmartDeviceMaker = new SmartDeviceMarker({ x: value.origin.x, y: value.origin.y, z: value.origin.z },
+             { x: 40, y: 40 }, 
+             value.smartDeviceId, 
+             value.smartDeviceType,
+             cloudData[value.smartDeviceId],
+             value.id
+             );
+      
+
+         this._markerSet.push(SmartDeviceMaker);
+      })
+   }
+
+   public decorate(context: DecorateContext): void {
+      this._markerSet.forEach(marker => {
+         marker.addDecoration(context);
+
+      })
+   }
+}
