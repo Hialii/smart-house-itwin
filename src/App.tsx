0cc215098fbd3056832dff158c5b8b721de49b23
Hiali <hialidias@gmail.com>
2023-09-23 21:16:32 -0300

    feat: add button function

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import Itwin from "./Itwin";
 import "./Principal.scss";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App: React.FC = () => {
    const [data, setData] = useState([
-      { text: "Temperature", value: 25 },
+      { text: "Temperature", value: 25 }, //valores so para teste 
       { text: "Shading", value: 0.6 },
       { text: "Charge", value: 1.89 }
    ]);
+
+   const fetchData = () => {
+     //atualizar os valores do data, com os valores da api
+   }
 
    return (
       <div className="principal-container">
@@ -28,7 +32,7 @@ const App: React.FC = () => {
                         </div>
                      </div>
                   ))}
-               <button className="button">Calculate</button>
+               <button className="button" onClick={fetchData}>Calculate</button>
                </div>
             </div>
             <div className="container-right"> 
